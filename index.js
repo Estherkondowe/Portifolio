6a8501bf7b91f58baa6625802aadcaa34f268fd2
Esther <Kondoweesther2@gamil.com>
2025-03-26 15:25:04 -0700

    implemented vanilla js functionalities

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+
 document.addEventListener('DOMContentLoaded', function () {
-                
+
                 const form = document.querySelector('form');
             
                 form.addEventListener('submit', function (e) {
@@ -74,6 +75,80 @@ document.addEventListener('DOMContentLoaded', function () {
                     const errorMessages = document.querySelectorAll('.error-message');
                     errorMessages.forEach(error => error.remove());
                 }
-            })
-       
+                const textE =document.querySelector('#name');
+                const words = ['Esther Kondowe','An aspiring Fullstack Developer' , 'also an Information Technology Student at Malawi University of Business and Applied Sciences'].map(word=> word +'.');
+        
 
+                let wordIndex =0;
+                let charIndex=0;
+
+                let addingchars= true;
+                let shouldWait= false;
+
+                let currentWord= words[wordIndex];
+                const updateCurrentWord =()=>{
+                    wordIndex ++;
+                    if(wordIndex===words.length){
+                        wordIndex=0;
+                    }
+                    currentWord=words[wordIndex]
+                }
+                const addChar =() =>{
+                    let currChar= currentWord[charIndex];
+                    const char =document.createElement('span');
+                    
+                    char.innerText=currChar;
+                    char.classList.add('char');
+                    textE.appendChild(char);
+                    charIndex++
+                    if (charIndex===currentWord.length){
+                        charIndex--;
+                      addingchars=false;
+                      shouldWait=true;
+                    }
+                }
+        const removeChar =()=> {
+        const char =textE.lastElementChild
+        if(char){
+            textE.removeChild(char);
+        }
+        
+        charIndex--
+        if (charIndex<0){
+            charIndex++;
+            addingchars=true;
+            updateCurrentWord();
+
+     } 
+}
+const typeRun=()=>{
+    const operation= addingchars ? addChar: removeChar
+    operation()
+        let timeout = addingchars? 200 : 100;
+        if(shouldWait){
+            timeout=1000
+            shouldWait=false;
+        }
+    setTimeout(typeRun,timeout)
+}
+setTimeout(typeRun,1500)
+
+const filterButtons = document.querySelectorAll('.filter-btn');
+    const projects = document.querySelectorAll('.projects');
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', () => { filterButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+            const filter = button.dataset.filter;
+            projects.forEach(project => {
+              if (filter ==='all' || project.dataset.category === filter) {
+                project.style.display = 'block'; // Show
+              } else {
+                project.style.display = 'none'; // Hide
+              }
+            });
+          });
+        });
+           
+     })
+     
